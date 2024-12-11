5017cb72326638a80cccfdb827fea2065f64ac37
ryantcipta <ryantika3@gmail.com>
2024-12-11 11:55:39 +0800

    perbaikan route 2

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('/', function () {
     return view('users.index');
 });
 
+
+
 Route::get('/dashboard', [AdminController::class, 'index']);
 
 Route::get('/upload', [AdminController::class, 'upload']);
@@ -30,6 +32,9 @@ Route::get('/upload/list',[AdminController::class, 'UploadList'])->name('upload.
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
 
 Route::get('/login', [AuthController::class, 'ShowLogin']);
 Route::post('/login', [AuthController::class, 'Login'])->name('auth.login');
